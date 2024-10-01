a6210319b9fdbad75f84635e9f380565cb8a3739
stephenJoester <hquanltvdn@gmail.com>
2024-10-01 22:09:19 +0700

    fix

--- Crawler/automatic_scripts.ps1
+++
@@ -1,9 +0,0 @@
-# Set the path to the notebook and output notebook
-$notebook_path = "D:\NgHoangQuan\Code_test\PBL7\Crawler\spotify_crawler.ipynb"
-$output_path = "D:\NgHoangQuan\Code_test\PBL7\Crawler\spotify_crawler_output.ipynb"
-
-# Change directory to the desired location
-Set-Location "D:\NgHoangQuan\Code_test\PBL7\Crawler"
-
-# Run papermill command
-papermill $notebook_path $output_path
